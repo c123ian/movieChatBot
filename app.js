c7cc3e7b20d3cf24360a40dbc99bd96bde80b412
HammerMorty <c123ian@gmail.com>
2017-11-15 15:12:47 +0000

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+//setup the 3 required dependecies
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+//////create seerver and define parsing strategies
+//const server = express();
+//server.use(bodyParser.urlencoded({
+  //  extended: true
+//}));
+
+//server.use(bodyParser.json());
+
+/////heroku Server
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
+
+// heroku Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+/////heroku server
+
+//define a ROUTE on the seerver
+server.post('/get-movie-details', function (req, res) {
+
+    let movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
+    let reqUrl = encodeURI('http://theapache64.xyz:8080/movie_db/search?keyword=' + movieToSearch);
+    http.get(reqUrl, (responseFromAPI) => {
+
+        responseFromAPI.on('data', function (chunk) {
+            let movie = JSON.parse(chunk)['data'];
+            let dataToSend = movieToSearch === 'The Godfather' ? 'I don\'t have the required info on that. Here\'s some info on \'The Godfather\' instead.\n' : '';
+            dataToSend += movie.name + ' is a ' + movie.stars + ' starer ' + movie.genre + ' movie, released in ' + movie.year + '. It was directed by ' + movie.director;
+
+            return res.json({
+                speech: dataToSend,
+                displayText: dataToSend,
+                source: 'get-movie-details'
+            });
+
+        });
+    }, (error) => {
+        return res.json({
+            speech: 'Something went wrong!',
+            displayText: 'Something went wrong!',
+            source: 'get-movie-details'
+        });
+    });
+});
+///setup server to listen
+//server.listen((process.env.PORT || 8000), function () {
+//    console.log("Server is up and running...");
+//});
